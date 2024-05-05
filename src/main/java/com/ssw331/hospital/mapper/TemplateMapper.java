982791c21de3f1b0d42f2f5fabff36cf40880d0c
YiR2002 <2245437308@qq.com>
2024-05-05 18:40:23 +0800

    add templatemapper

---
+++ src/main/java/com/ssw331/hospital/mapper/TemplateMapper.java
@@ -0,0 +1,9 @@
+package com.ssw331.hospital.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ssw331.hospital.dto.Template;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface TemplateMapper extends BaseMapper<Template>{
+}
